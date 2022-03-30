b9f0c4ce8855c78b101ee30ad991db4e6a448d5a
GregorySchwing <gschwing@uno.edu>
2022-03-30 17:33:00 -0500

    lin for

--- src/matchgpu.h
+++ src/matchgpu.h
@@ -37,6 +37,7 @@ class GraphMatchingGPU : public GraphMatching
 		virtual ~GraphMatchingGPU();
 		
 		virtual void performMatching(std::vector<int> &, cudaEvent_t &, cudaEvent_t &) const = 0;
+		virtual void performMatching(std::vector<int> &match , cudaEvent_t &a, cudaEvent_t &b, std::vector<int> &fll, std::vector<int> &heads, std::vector<int> &tails) const override;
 		virtual void performMatching(std::vector<int> &match , cudaEvent_t &a, cudaEvent_t &b, std::vector<int> &fll, std::vector<int> &heads, std::vector<int> &tails) {
 			performMatching(match, a, b);
 		}
@@ -66,7 +67,7 @@ class GraphMatchingGeneralGPURandom : public GraphMatchingGPU
 		virtual void performMatching(std::vector<int> &, cudaEvent_t &, cudaEvent_t &) const{
 			std::cout << "wrong method!" << std::endl;
 		}
-		virtual void performMatching(std::vector<int> &match, cudaEvent_t &t1, cudaEvent_t &t2, std::vector<int> &hfll, std::vector<int> &hheads, std::vector<int> &htails) const;
+		virtual void performMatching(std::vector<int> &match, cudaEvent_t &t1, cudaEvent_t &t2, std::vector<int> &hfll, std::vector<int> &hheads, std::vector<int> &htails) const override;
 
 };
 
