4e19c037e02cb46cb16845e55f10a1a905878a94
GregorySchwing <gschwing@uno.edu>
2022-03-30 11:00:04 -0500

    A

--- bin/match.cpp
+++ bin/match.cpp
@@ -36,6 +36,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <boost/iostreams/filtering_stream.hpp>
 #include <boost/iostreams/filter/bzip2.hpp>
 
+#include <string>
+
 using namespace std;
 using namespace tbb;
 using namespace mtc;
