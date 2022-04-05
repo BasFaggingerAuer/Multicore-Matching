1ad2c412fa955e9b74483d75626c905b8b231796
GregorySchwing <gschwing@uno.edu>
2022-04-05 18:41:21 -0500

    y

--- src/matchgpu.cu
+++ src/matchgpu.cu
@@ -1017,9 +1017,9 @@ void GraphMatchingGeneralGPURandom::performMatching(vector<int> &match, cudaEven
 
 		for (int i = 0; i < NR_MATCH_ROUNDS; ++i)
 		{
-			printf("Match round %d\n", i);
 			cudaDeviceSynchronize();
 			checkLastErrorCUDA(__FILE__, __LINE__);
+			printf("Match round %d\n", i);
 			gSelect<<<blocksPerGrid, threadsPerBlock>>>(dmatch, dsense, dforwardlinkedlist, dbackwardlinkedlist, graph.nrVertices, rand());
 			cudaDeviceSynchronize();
 			checkLastErrorCUDA(__FILE__, __LINE__);
